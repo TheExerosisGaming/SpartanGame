bbdd0d594afa64ffc91ed0cee01b9a513843e4b3
Exerosis <exerosis@gmail.com>
2015-07-31 11:26:43 -0400

    Update 3.0.2

--- app/src/main/java/me/exerosis/spartangame/util/Redis.java
+++ app/src/main/java/me/exerosis/spartangame/util/Redis.java
@@ -10,13 +10,23 @@ import redis.clients.jedis.Jedis;
  * Created by The Exerosis on 7/29/2015.
  */
 public class Redis {
+    public static Jedis jedis;
+    public static int port = SettingsActivity.DEFAULT_PORT;
+    public static String host = SettingsActivity.DEFAULT_HOST;
+
     public static Jedis get(Bundle bundle) {
-        Log.e("CONNECTING", bundle.getString(SettingsActivity.ARGS_HOST) + ":" + bundle.getInt(SettingsActivity.ARGS_PORT));
-        Jedis jedis = new Jedis(bundle.getString(SettingsActivity.ARGS_HOST), bundle.getInt(SettingsActivity.ARGS_PORT));
-        try {
-            jedis.connect();
-        } catch (Exception e) {
+        String newHost = bundle.getString(SettingsActivity.ARGS_HOST);
+        int newPort = bundle.getInt(SettingsActivity.ARGS_PORT);
+        if(!newHost.equals(host) || port != newPort) {
+            jedis = new Jedis(newHost, newPort);
+            host = newHost;
+            port = newPort;
+            try {
+                jedis.connect();
+            } catch (Exception e) {
+            }
         }
+
         return jedis;
     }
 }
